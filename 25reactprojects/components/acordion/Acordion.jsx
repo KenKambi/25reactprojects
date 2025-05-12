7d06a1260e820be2d32e005186e7497422edc650
Ken Kambi <kenkambi43@gmail.com>
2025-05-12 14:53:03 +0300

    style.css

--- 25reactprojects/components/acordion/Acordion.jsx
+++ 25reactprojects/components/acordion/Acordion.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
-
 export default function Acordion(props) {
-  const [ans, setAns] = useState(false)
+  const [ans, setAns] = useState(false);
 
-  function showAns (){
-    setAns(prevAns => !prevAns)
+  function showAns() {
+    setAns((prevAns) => !prevAns);
   }
 
   return (
-    <>
-        <div onClick={showAns}> {props.acordion.question} </div>
-        { ans && <div> {props.acordion.answer} </div>}
-        <hr />
-    </>
+    <main className="main-section">
+      <div className="question" onClick={showAns}>
+        {" "}
+        {props.acordion.question}
+        {ans ? <span > &#65293; </span> : <span >  &#65291; </span>}
+      </div>
+
+      {ans && <div className="answer"> {props.acordion.answer} </div>}
+      <hr />
+    </main>
   );
 }
--- 25reactprojects/src/App.jsx
+++ 25reactprojects/src/App.jsx
@@ -1,13 +1,14 @@
 import Acordion from "../components/acordion/Acordion";
 import accordionData from "../components/acordion/data";
 
+
 function App() {
   const data = accordionData.map(function (acordion) {
     return <Acordion key={acordion.id} acordion={acordion} />;
   });
   return (
     <>
-      <h3>I am an acordion</h3>
+      <h3>I am an Acordion</h3>
       {data}
     </>
   );
