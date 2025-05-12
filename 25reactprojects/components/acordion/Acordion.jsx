9b98d073e32b6ab90bb6f110d1a4105639eee40d
Ken Kambi <kenkambi43@gmail.com>
2025-05-12 12:57:46 +0300

    onclick function

--- 25reactprojects/components/acordion/Acordion.jsx
+++ 25reactprojects/components/acordion/Acordion.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import accordionData from "./data";
 
 export default function Acordion() {
   console.log(accordionData);
 
+  const [ans, setAns] = useState("")
+
   const data = accordionData.map(function (acordion) {
     return (
-      <div key={acordion.id}>
+      <div key={acordion.id} onClick={showAns} >
         {" "}
         {acordion.question}
         <div key={acordion.id}> {acordion.answer} </div>
+        <hr />
       </div>
     );
   });
+
+  function showAns (){
+    console.log("clicked")
+    setAns(accordionData.answer)
+  }
 
   return (
     <>
